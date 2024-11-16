01c4a4bfd11db1e227e96651df16975fa1ad79d6
Nikita Krenov <nikitabaram@gmail.com>
2024-11-16 12:11:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace app_c_
+{
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("It's easy to win forgiveness for being wrong;\r\nbeing right is what gets you into real trouble! \r\n");
+    //        Console.WriteLine("Bjarne Stroustrup");
+    //    }
+    //}
+
+    //internal class task2
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[] numbers = new int[5];
+    //        int sum = 0;
+    //        int product = 1;
+    //        int min = numbers[0];
+    //        int max = numbers[1];
+
+    //        for (int i = 0; i < 5; i++)
+    //        {
+    //            Console.Write("Enter number: ");
+    //            numbers[i] = Convert.ToInt32(Console.ReadLine());
+    //            sum += numbers[i];
+    //        }
+
+    //        foreach (int number in numbers)
+    //        {
+    //            product *= number;
+    //            if (number < min)
+    //            {
+    //                min = number;
+    //            }
+    //            if (number > max)
+    //            {
+    //                max = number;
+    //            }
+    //        }
+
+    //        Console.WriteLine("Sum: " + sum);
+    //        Console.WriteLine("Product: " + product);
+    //        Console.WriteLine("Min: " + min);
+    //        Console.WriteLine("Max: " + max);
+
+    //    }
+    //}
+
+    internal class task3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a 6 numbers: ");
+            Console.ReadLine();
+
+        }
+    }
+}
