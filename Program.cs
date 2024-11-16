8986b35f118513e31297dd77a93fbb9e26ada8ef
Nikita Krenov <nikitabaram@gmail.com>
2024-11-16 12:42:29 +0200

    Refactor task5 and add commented-out code for tasks 3 & 4
    Added commented-out code for task3 (reverse 6-digit number) and
    task4 (print sequence between A and B). Refactored task5 to prompt
    user for line length, fill character, and direction, then print
    line accordingly. Adjusted closing braces for task5 class and
    namespace.

--- Program.cs
+++ Program.cs
@@ -47,13 +47,95 @@
     //    }
     //}
 
-    internal class task3
+    //internal class task3
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.Write("Enter a 6 numbers: ");
+    //        string input = Console.ReadLine();
+
+    //        if (input.Length == 6 && int.TryParse(input, out int number))
+    //        {
+    //            char[] reversedArray = input.ToCharArray();
+    //            Array.Reverse(reversedArray);
+    //            string reversedNumber = new string(reversedArray);
+
+    //            Console.WriteLine($"Reversed number: {reversedNumber}");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Invalid input");
+    //        }
+
+    //    }
+    //}
+
+    //internal class task4
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.Write("Enter A: ");
+    //        int A = int.Parse(Console.ReadLine());
+    //        Console.Write("Enter B: ");
+    //        int B = int.Parse(Console.ReadLine());
+
+    //        if (A > 0 && B > 0)
+    //        {
+    //            for (int i = A; i <= B; i++)
+    //            {
+    //                for (int j = 0; j < i; j++)
+    //                {
+    //                    Console.Write(i + " ");
+    //                }
+    //                Console.WriteLine();
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Invalid input");
+    //        }
+
+    //    }
+    //}
+
+    internal class task5
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a 6 numbers: ");
-            Console.ReadLine();
+            Console.Write("Enter the length of the line: ");
+            int length = int.Parse(Console.ReadLine());
 
+            Console.Write("Enter the fill character: ");
+            char fillChar = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.Write("Enter the direction (horizontal/vertical): ");
+            string direction = Console.ReadLine().ToLower();
+
+            Console.WriteLine("\nResult:");
+
+            if (length > 0 && (direction == "horizontal" || direction == "vertical"))
+            {
+                if (direction == "horizontal")
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        Console.Write(fillChar);
+                    }
+                    Console.WriteLine();
+                }
+                else if (direction == "vertical")
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        Console.WriteLine(fillChar);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
         }
     }
-}
+}   
